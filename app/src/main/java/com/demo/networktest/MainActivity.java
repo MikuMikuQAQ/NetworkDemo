16b84243d98b72f2e45b8533d23729519d936de4
MikuMikuQAQ <1874301384@qq.com>
2018-08-16 17:38:06 +0800

    添加了解析JSON的功能

--- app/src/main/java/com/demo/networktest/MainActivity.java
+++ app/src/main/java/com/demo/networktest/MainActivity.java
@@ -6,12 +6,14 @@ import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatTextView;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.zhy.http.okhttp.https.HttpsUtils;
+import okhttp3.*;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
@@ -19,6 +21,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,10 +29,10 @@ public class MainActivity extends AppCompatActivity {
     AppCompatTextView textView;
 
     @OnClick({R.id.send_button})
-    public void onClick(View view){
+    public void onClick(View view) {
         switch (view.getId()) {
             case R.id.send_button:
-                Log.e("TAG", "onClick: " );
+                Log.e("TAG", "onClick: ");
                 sendHttp();
                 break;
             default:
@@ -44,11 +47,12 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
     }
-/*
-* 方法一：
-* HttpsURLConnection方法实现网络请求
-*
-* */
+
+    /*
+     * 方法一：
+     * HttpsURLConnection方法实现网络请求
+     *
+     * */
     /*private void sendHttp(){
         new Thread(new Runnable() {
             @Override
@@ -87,33 +91,90 @@ public class MainActivity extends AppCompatActivity {
             }
         }).start();
     }*/
-/*
-* 方法二
-* OkHttp实例
-*
-* */
-    private void sendHttp(){
-        new Thread(new Runnable() {
+    /*
+     * 方法二
+     * OkHttp实例
+     *
+     * */
+    private void sendHttp() {
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    OkHttpClient client = new OkHttpClient(); //创建OkHttpClient实例
+//                    Request request = new Request.Builder()
+//                            .url("http://guolin.tech/api/china/") //将地址修改为json地址
+//                            .build(); //创建Request对象
+//                    Response response = client.newCall(request).execute(); //newCall请求数据，Response对象储存服务器返回数据
+//                    //showLineText(response.body().string());
+//                    resolveJson(response.body().string());
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }).start();
+
+        HttpUtil.sendOkHttp(new Callback() {
             @Override
-            public void run() {
-                try {
-                    OkHttpClient client = new OkHttpClient(); //创建OkHttpClient实例
-                    Request request = new Request.Builder().url("https://www.baidu.com").build(); //创建Request对象
-                    Response response = client.newCall(request).execute(); //newCall请求数据，Response对象储存服务器返回数据
-                    showLineText(response.body().string());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+            public void onFailure(Call call, IOException e) { //返回失败信息
+                runOnUiThread(new Runnable() { //从子线程回到主线程中更新UI
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "加载失败！", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
-        }).start();
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException { //返回成功信息
+                Gson gson = new Gson();
+                List<Cities> citiesList = gson.fromJson(response.body().string(), new TypeToken<List<Cities>>() {
+                }.getType());
+                String str = "";
+                for (Cities cities : citiesList) {
+                    Log.e("TAG",  "\n" + "resolveJson: " + "id:" + cities.getId() + "\t\t\t" + "city:" + cities.getName());
+                    str = str + "id:" + cities.getId() + "\t\t\t" + "city:" + cities.getName() + "\n";
+                }
+                final String finalStr = str;
+                runOnUiThread(new Runnable() { //从子线程回到主线程中更新UI
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "加载成功", Toast.LENGTH_SHORT).show();
+                        textView.setText(finalStr);
+                    }
+                });
+            }
+        });
     }
 
-    private void showLineText(final String str){
+    /*private void showLineText(final String str){
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 textView.setText(str);
             }
         });
+    }*/
+
+/*    private void resolveJson(String data) {
+        Gson gson = new Gson();
+        List<Cities> citiesList = gson.fromJson(data, new TypeToken<List<Cities>>() {
+        }.getType());
+        for (Cities cities : citiesList) {
+//            textView.setText("id:" + cities.getId() + "\n" + "city:" + cities.getName());
+            Log.e("TAG", "resolveJson: " + cities.getId() + "\n" + "city:" + cities.getName());
+        }
+    }*/
+
+    static class HttpUtil {
+
+        public static void sendOkHttp(final okhttp3.Callback callback) {
+            OkHttpClient client = new OkHttpClient(); //创建OkHttpClient实例
+            Request request = new Request.Builder()
+                    .url("http://guolin.tech/api/china/") //将地址修改为json地址
+                    .build(); //创建Request对象
+            client.newCall(request).enqueue(callback); //newCall请求数据,并回调接口
+        }
     }
+
 }
